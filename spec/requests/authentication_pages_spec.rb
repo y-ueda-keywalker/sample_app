e1bd3513da7768a41579fbe7c6df719cdfd2dab1
y_ueda <y_ueda@keywalker.jp>
2014-10-03 19:07:06 +0900

    Flashのバグを検出するテストの追加

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -11,7 +11,12 @@ describe "Authentication" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid')}
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
     end
 
     describe "with valid information" do
@@ -29,5 +34,4 @@ describe "Authentication" do
       it { should_not have_link('Sign in', href:signin_path) }
     end
   end
-
 end
