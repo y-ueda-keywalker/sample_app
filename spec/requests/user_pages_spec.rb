34a1b7da4f6d2116431d372e2a378cf76e96aac9
y_ueda <y_ueda@keywalker.jp>
2014-10-10 18:01:55 +0900

    編集失敗のテストを通過(9.1.2)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -53,4 +53,25 @@ describe "UserPages" do
       end
     end
   end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
+    end
+    
+  end
 end
